a97b288a0c3033352dfe191359da007749f3857c
Nilesh <nilesh.bitcot@gmail.com>
2022-07-06 17:02:21 +0530

    Login form

--- functions.php
+++ functions.php
@@ -15,3 +15,48 @@ function bootstrap_basic4_child_enqueue_styles() {
 /*
  * Your code goes below
  */
+
+
+// wdt_login_form
+add_shortcode( 'wdt_login_form', 'wdt_login_form_shortcode_func' );
+function wdt_login_form_shortcode_func( $atts ) {
+    // $attributes = shortcode_atts( array(
+    //     'title' => false,
+    //     'limit' => 4,
+    // ), $atts );
+     
+    ob_start();
+    ?>
+    <h2>Login form</h2>
+    <form method="post" name="wdt-login_form">
+        <input type="text" name="user_login" id="user_login" placeholder="Email/username">
+        <input type="password" name="user_pass" id="user_pass" placeholder="password">
+        <input type="hidden" name="action" value="wdt_custom_login_action">
+        <input type="submit" name="login" value="Login">
+    </form>
+ 
+    <?php
+    return ob_get_clean();
+}
+
+add_action('init', function(){
+    if( isset($_POST['action']) && $_POST['action'] == 'wdt_custom_login_action' ){
+        $userdata = array(
+            'user_login'    => $_POST['user_login'],
+            'user_password' => $_POST['user_pass'],
+            'remember'      => false
+        );
+
+        // if(filter_var($userdata['user_login'], FILTER_VALIDATE_EMAIL)) {}
+     
+        $user = wp_signon( $userdata, false );
+     
+        if ( is_wp_error( $user ) ) {
+            echo $user->get_error_message();
+        }
+        else{
+            wp_redirect( home_url() ); exit;
+        }
+    }
+});
+

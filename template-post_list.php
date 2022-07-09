c083345e6a4c6f01006f898ef8488b90e3894400
Nilesh <nilesh.bitcot@gmail.com>
2022-07-09 12:09:21 +0530

    template with post loop and pagination

--- template-post_list.php
+++ template-post_list.php
@@ -16,18 +16,18 @@ get_header();
         'paged' => $paged
     );
 
+    $posts = new WP_Query( $args );
+
+    if($posts->post_count > 0){
+        foreach($posts->posts as $post){
+            echo '<h4>'.get_the_title($post->ID).'</h4>';
+            echo '<br>';
+        }
+    }
+
     /**
      * Pagination with page numbers with WP_Query
      */
-
-    // $posts = new WP_Query( $args );
-
-    // if($posts->post_count > 0){
-    //     foreach($posts->posts as $post){
-    //         echo '<h4>'.get_the_title($post->ID).'</h4>';
-    //         echo '<br>';
-    //     }
-    // }
 
     // next_posts_link( '&larr; Older posts', $posts->max_num_pages);
     // previous_posts_link( 'Newer posts &rarr;' );
@@ -35,15 +35,9 @@ get_header();
     /**
      * Pagination with page numbers with query_posts()
      */
-    query_posts($args);
 
-    if ( have_posts() ) : 
-        while (have_posts()) : the_post();
-            echo '<h4>'.the_title().'</h4>';
-            echo '<br>';
-        endwhile; 
-    endif;
-
+    $GLOBALS['wp_query']->max_num_pages = $posts->max_num_pages;
+    
     the_posts_pagination( array( 'mid_size'  => 2 ) );
     echo '</div>';
 
